2b0eda91ea9ec6573e77f7a2a29c91f2daad3c4c
Maksim Bolotin <vk07122012@gmail.com>
2024-01-04 16:05:03 +0300

    Написаны тесты на методы класс Stack с использованием unittest.

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        """
+        Метод, который будет вызываться перед каждым тестом
+        """
+        self.stack = Stack()
+
+    def test_push_and_pop(self):
+        """
+        Проверка методов push и pop
+        """
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.pop(), 1)
