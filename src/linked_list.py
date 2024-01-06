be70e8ac9119a3a8ac0fc93c1a2902db16747fb6
Maksim Bolotin <vk07122012@gmail.com>
2024-01-06 22:33:50 +0300

    Реализованы классы Node и LinkedList. Написаны методы insert_beginning и insert_at_end. Обновил метод __str__, т.к. не проходил тест в mail.py.

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,24 +1,43 @@
 class Node:
-    """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node):
+        """Конструктор класса Node"""
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+        self.all = []
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data, self.head)
+        self.head = node
+        self.all.append(data)
+        if self.tail is None:
+            self.tail = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data, None)
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
+        self.all.append(data)
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
         node = self.head
         if node is None:
-            return str(None)
+            return "None"
 
         ll_string = ''
         while node:
@@ -26,4 +45,4 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.strip()
